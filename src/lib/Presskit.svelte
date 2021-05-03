6496ae149b7b91c1d94afdd5a45aaa54215df02e
Brent Brinkley <brentbrinkley@gmail.com>
2021-05-02 21:53:26 -0400

    Added Presskit page

--- src/lib/Presskit.svelte
+++ src/lib/Presskit.svelte
@@ -1,1 +1,80 @@
-<div class="text-fkWhite text-4xl">Coming Soon</div>
+<!-- <div class="text-fkWhite text-4xl">Coming Soon</div> -->
+<div class="text-fkWhite mt-6">
+  <div class="lg:mr-64">
+    <h1 class="text-4xl font-headline mb-1">Description</h1>
+    <p class="pl-3 font-body mb-4 font-light">
+      FormKey is the first MIDI Controller that makes you a better musician as
+      you use it. Utilizing shapes to define notes and colors to define octaves,
+      FormKey's beautiful isomorphic keys aid in pattern recognition and makes
+      discovering new chords easier than it's ever been.
+    </p>
+  </div>
+
+  <div>
+    <h1 class="text-4xl font-headline mb-1">Features</h1>
+    <ul class="list-disc pl-7 font-body mb-4 font-light">
+      <li>The first instrument to use the Form music language</li>
+      <li>Simplified MIDI Controller that focuses on playability</li>
+      <li>Beautiful user interface that's designed to stay out of your way</li>
+      <li>
+        Isomorphic keyboard that makes playing chords and recognizing note
+        relationships a breeze
+      </li>
+    </ul>
+  </div>
+
+  <div class="grid grid-cols-2 gap-2">
+    <div>
+      <h1 class="text-4xl font-headline mb-1">Developer</h1>
+      <p class="pl-3 font-body mb-4 font-light">
+        Polyhedra LLC based in RDU, North Carolina
+      </p>
+    </div>
+
+    <div class="row-span-2">
+      <h1 class="text-4xl font-headline mb-1">Social Links</h1>
+      <div class="pl-3 flex flex-col font-body mb-4 font-light">
+        <a
+          class="hover:opacity-80 transition duration-300"
+          href="https://www.facebook.com/wearepolyhedra">facebook</a
+        >
+        <a
+          class="hover:opacity-80 transition duration-300"
+          href="https://www.instagram.com/wearepolyhedra/">instagram</a
+        >
+        <a
+          class="hover:opacity-80 transition duration-300"
+          href="https://twitter.com/WeArePolyhedra">twitter</a
+        >
+        <a
+          class="hover:opacity-80 transition duration-300"
+          href="https://www.buymeacoffee.com/S8VWrdFUg">bmc</a
+        >
+      </div>
+    </div>
+
+    <div>
+      <h1 class="text-4xl font-headline mb-1">Release Date</h1>
+      <p class="pl-3 font-body mb-4 font-light">October 14, 2020</p>
+    </div>
+
+    <div>
+      <h1 class="text-4xl font-headline mb-1">Platform</h1>
+      <p class="pl-3 font-body mb-4 font-light">iPad iOS 14 and later</p>
+    </div>
+
+    <div>
+      <h1 class="text-4xl font-headline mb-1">Assets</h1>
+      <a
+        class="pl-3 font-body mb-4 font-light hover:opacity-80 transition duration-300"
+        href="https://www.dropbox.com/sh/iwn2pv03n0sdtjn/AADGiLr-Wuf-psek7erPnKZia?dl=0"
+        >Open Dropbox</a
+      >
+    </div>
+
+    <div>
+      <h1 class="text-4xl font-headline mb-1">Price</h1>
+      <p class="pl-3 font-body mb-4 font-light">$2.99 USD introductory price</p>
+    </div>
+  </div>
+</div>
