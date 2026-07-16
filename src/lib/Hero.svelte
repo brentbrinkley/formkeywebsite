607bb73aa2c5888e9958535399dfe0532cd01917
Brent Brinkley <brentbrinkley@gmail.com>
2026-07-16 18:56:54 -0400

    Fit hero iPad within viewport and keep footer in view on laptop

--- src/lib/Hero.svelte
+++ src/lib/Hero.svelte
@@ -3,11 +3,11 @@
   import Card from './Card.svelte'
 </script>
 
-<div class="flex items-center justify-center justify-items-center">
+<div class="flex items-center justify-center justify-items-center lg:min-h-[calc(100vh_-_10rem)]">
   <div class="lg:w-1/4">
     <Card />
   </div>
-  <div class="hidden lg:block lg:w-3/4 pl-2 pt-2 lg:-mt-6">
-    <IpadVideo imgClass="drop-shadow-2xl" />
+  <div class="hidden lg:block lg:w-3/4 pl-2 pt-2">
+    <IpadVideo imgClass="drop-shadow-2xl lg:max-h-[calc(100vh_-_11rem)] lg:mx-auto" />
   </div>
 </div>
